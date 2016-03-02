1da1d4bab19be1d9f149c23c95c3486b835bcc8a
Tom Maher <tmaher@github.com>
2016-03-02 11:56:33 -0800

    wip

--- go-ssh-add.go
+++ go-ssh-add.go
@@ -8,12 +8,13 @@ import "crypto/x509"
 import "encoding/pem"
 import "golang.org/x/crypto/ssh"
 import "golang.org/x/crypto/ssh/agent"
-import "github.com/lunixbochs/go-keychain"
-
+// import "github.com/lunixbochs/go-keychain"
+import "github.com/keybase/go-keychain"
 
 
 func main() {
   fmt.Printf("hello, %s\n", ssh.CertAlgoRSAv01)
+
   var key_path = "/Users/tmaher/.ssh/id_rsa"
   var k agent.AddedKey
   k.Comment = "howdy"
@@ -23,8 +24,26 @@ func main() {
     os.Exit(1)
   }
   var block, _ = pem.Decode(key_buffer)
-  var pw, _ = keychain.Find("SSH", key_path)
+
+  // lunixbochs code
+  // var pw, _ = keychain.Find("SSH", key_path)
   // fmt.Printf("pw: %s\n", pw)
+
+  // keybase code
+  var pw = ""
+  accts, err := keychain.GetGenericPasswordAccounts("SSH")
+  for _, acct := range accts {
+    fmt.Printf("found account %s\n", acct)
+    pw_raw, err := keychain.GetGenericPassword("SSH", acct, "", "")
+    if err != nil {
+      fmt.Printf("Noooo can't find pw for %s\n", acct)
+    } else {
+      pw = string(pw_raw)
+      fmt.Printf("%s: %s\n", acct, pw)
+    }
+  }
+  os.Exit(1)
+
   var block_plaintext, _ = x509.DecryptPEMBlock(block, []byte(pw))
   var add_key agent.AddedKey
   add_key.PrivateKey, err = x509.ParsePKCS1PrivateKey(block_plaintext)
